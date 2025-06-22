a1f4a559e07b393ed0c5def5d8716ea4b51646be
DylanJDombrowski <dyland601@gmail.com>
2025-06-22 15:37:14 -0400

    removed secret

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,17 @@
+// lib/supabase.ts
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const supabaseUrl = "https://zxyavwkcmpqonxrfvogl.supabase.co"; // Paste your URL here
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4eWF2d2tjbXBxb254cmZ2b2dsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2MTcxMzQsImV4cCI6MjA2NjE5MzEzNH0.XdJdCjB9E-fP0mVJXoqWP4NAgurOpoLQ5oihN3AYLD0"; // Paste your anon key here
+// Read the variables from process.env
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+// Add a check to ensure the variables are loaded
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key must be provided in .env");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
